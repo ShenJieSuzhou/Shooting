875d93c18dff9e9e0a2d66103b513288a7fe9188
Code by a Cat <745689122@qq.com>
2023-01-04 13:26:11 +0800

    Initialize project and prepare asstes.

---
+++ Shooting/Source/Shooting/ShootingGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShootingGameMode.h"
+#include "ShootingHUD.h"
+#include "ShootingCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AShootingGameMode::AShootingGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AShootingHUD::StaticClass();
+}
