e45843c721216c134e0a56408341aedfad0d99e5
Code by a Cat <745689122@qq.com>
2023-01-09 13:40:08 +0800

    Fix knife position bug.

--- Shooting/Source/Shooting/WeaponKnife.cpp
+++ Shooting/Source/Shooting/WeaponKnife.cpp
@@ -8,7 +8,7 @@ AWeaponKnife::AWeaponKnife()
     // get knife mesh
 	if(FP_Gun)
 	{
-		auto assetSkeletal = ConstructorHelpers::FObjectFinder<USkeletalMesh>(TEXT("SkeletalMesh'/Game/ShootingPawn/rigged/Knife_Rigged.Knife_Rigged'"));
+		auto assetSkeletal = ConstructorHelpers::FObjectFinder<USkeletalMesh>(TEXT("SkeletalMesh'/Game/FPS_Weapon_Bundle/Weapons/Meshes/M9_Knife/SK_M9_Knife.SK_M9_Knife'"));
 		if(assetSkeletal.Succeeded())
 		{
 			FP_Gun->SetSkeletalMesh(assetSkeletal.Object);
