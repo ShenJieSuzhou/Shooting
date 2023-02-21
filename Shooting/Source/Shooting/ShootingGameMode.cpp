dc93800327c71cc8704822eb76bbf7a60d91d2d5
ShenJieSuzhou <745689122@qq.com>
2023-02-21 22:31:18 +0800

    完善枪械换弹声音

--- Shooting/Source/Shooting/ShootingGameMode.cpp
+++ Shooting/Source/Shooting/ShootingGameMode.cpp
@@ -9,7 +9,8 @@ AShootingGameMode::AShootingGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("Blueprint'/Game/ShootingPawn/Blueprints/FPSPlayer_BP.FPSPlayer_BP'"));
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("Blueprint'/Game/ShootingPawn/Blueprints/FPSPlayer_BP.FPSPlayer_BP'"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
